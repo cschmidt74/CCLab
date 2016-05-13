4e05291206e1082a9cf80b7fdce832710d511baf
Luqzn <lukashutfless@yahoo.de>
2016-05-13 18:44:55 +0200

    Overwrite Lexer.cpp, Lexer.h
    - Semicolon on Line End
    - Keywords

--- p1/GoLangParser/Lexer.cpp
+++ p1/GoLangParser/Lexer.cpp
@@ -16,6 +16,7 @@
 Lexer::Lexer() {
     lines.clear();
     keywords.clear();
+    operators.clear();
     tokens.clear();
     tokenCount = 0;
 }
@@ -34,28 +35,42 @@ bool Lexer::readSourcecode(std::string filepath) {
         lines.push_back(line);
     }
     filestream.close();
-    return true;
 }
 
 /*
  * Prints out each line of the sourcecode member variable
  */
 void Lexer::printSourcecode() {
-    for (unsigned i = 0; i < lines.size(); i++) std::cout << std::setw(4) << std::left << i+1 << lines[i] << std::endl;
+    for (int i = 0; i < lines.size(); i++) std::cout << std::setw(4) << std::left << i + 1 << lines[i] << std::endl;
 }
 
 /*
  * Calls splitLine() for every element in lines member variable
  */
 int Lexer::createTokens() {
-    for (unsigned i = 0; i < lines.size(); i++) {
-        if (!splitLine(lines[i], i + 1)) return i + 1;
+    for (int i = 0; i < lines.size(); i++) {
+        if (!splitLine(lines[i], i + 1)) {
+            return i + 1;
+        }
     }
     return 0;
 }
 
+/*
+ * Checks if a semicolon on line end is needed
+ */
+bool Lexer::checkForSemicolonOnLineEnd(Token t) {
+    if (t.getType() == "IDENTIFIER" || t.getType() == "LITERAL" || t.getType() == "NUMBER") return true;
+    else if (t.getType() == "BRACKET" && (t.getValue() == "}" || t.getValue() == "]" || t.getValue() == ")")) return true;
+    else if (t.getValue() == "KEYWORD" && (t.getValue() == "break" || t.getValue() == "continue" || t.getValue() == "fallthrough" || t.getValue() == "return")) return true;
+    else return false;
+}
+
+/*
+ * Returns the adress of the next token based on tokenCount
+ */
 bool Lexer::getNextToken(Token& t) {
-    if(tokenCount<tokens.size()) {
+    if (tokenCount < tokens.size()) {
         t = tokens[tokenCount++];
         return true;
     } else {
@@ -64,6 +79,9 @@ bool Lexer::getNextToken(Token& t) {
     }
 }
 
+/*
+ * Set tokenCount to 0
+ */
 void Lexer::resetTokenCount() {
     tokenCount = 0;
 }
@@ -86,6 +104,12 @@ bool Lexer::splitLine(std::string line, int lineNumber) {
             data.pop_back();
         }
 
+        // handle semicolon
+        if (currentChar == ';') {
+            Token t("TERMINATOR", ";", lineNumber);
+            tokens.push_back(t);
+        }
+
         // handle comments
         if (currentChar == '/') {
             if (!data.empty()) {
@@ -95,8 +119,7 @@ bool Lexer::splitLine(std::string line, int lineNumber) {
             }
             return false; //comment failure: single / not valid
         }
-
-        // handle if the next token starts with a letter
+            // handle if the next token starts with a letter
         else if (isLetter(currentChar)) {
             tokenValue = currentChar;
             if (!data.empty()) {
@@ -117,8 +140,7 @@ bool Lexer::splitLine(std::string line, int lineNumber) {
                 tokens.push_back(t);
             }
         }
-
-        // handle if the next token starts with a number
+            // handle if the next token starts with a number
         else if (isNumber(currentChar)) {
             tokenValue = currentChar;
             if (!data.empty()) {
@@ -129,29 +151,26 @@ bool Lexer::splitLine(std::string line, int lineNumber) {
                     currentChar = data.back();
                     data.pop_back();
                 }
-                if(!isSpace(currentChar)) return false;
+                if (!isSpace(currentChar)) return false;
             }
             Token t("NUMBER", tokenValue, lineNumber);
             tokens.push_back(t);
         }
-
-        // handle brackets
+            // handle brackets
         else if (isBracket(currentChar)) {
             tokenValue = currentChar;
             currentChar = ' ';
             Token t("BRACKET", tokenValue, lineNumber);
             tokens.push_back(t);
         }
-
-        // handle funtioncall
+            // handle funtioncall
         else if (currentChar == '.') {
             tokenValue = currentChar;
             currentChar = ' ';
             Token t("FUNCTIONCALL", tokenValue, lineNumber);
             tokens.push_back(t);
         }
-
-        // handle literals
+            // handle literals
         else if (currentChar == '"') {
             tokenValue = currentChar;
             if (!data.empty()) {
@@ -171,10 +190,18 @@ bool Lexer::splitLine(std::string line, int lineNumber) {
                 tokens.push_back(t);
             }
         }
-        
-        else if(!isSpace(currentChar))return false;
+            // handle chars who are out of context
+        else if (!isSpace(currentChar))return false;
 
     }
+
+    if (!tokens.empty()) {
+        if (checkForSemicolonOnLineEnd(tokens.back())) {
+            Token t("TERMINATOR", ";", lineNumber);
+            tokens.push_back(t);
+        }
+    }
+
     return true;
 }
 
@@ -182,7 +209,7 @@ bool Lexer::splitLine(std::string line, int lineNumber) {
  * Prints out each token in the tokens member variable
  */
 void Lexer::printTokens() {
-    for (unsigned i = 0; i < tokens.size(); i++) {
+    for (int i = 0; i < tokens.size(); i++) {
         std::cout << tokens[i].getType() << " on line " << tokens[i].getLine() << " has the value: " << tokens[i].getValue() << std::endl;
     }
 }
@@ -192,20 +219,62 @@ void Lexer::printTokens() {
  */
 void Lexer::initKeywords() {
     keywords.clear();
-    keywords.push_back("package");
-    keywords.push_back("import");
+    keywords.push_back("break");
+    keywords.push_back("default");
     keywords.push_back("func");
-    keywords.push_back("int");
+    keywords.push_back("interface");
+    keywords.push_back("select");
+    keywords.push_back("case");
+    keywords.push_back("defer");
+    keywords.push_back("go");
+    keywords.push_back("map");
+    keywords.push_back("struct");
+    keywords.push_back("chan");
+    keywords.push_back("else");
+    keywords.push_back("goto");
+    keywords.push_back("package");
+    keywords.push_back("switch");
+    keywords.push_back("const");
+    keywords.push_back("fallthrough");
+    keywords.push_back("if");
+    keywords.push_back("range");
+    keywords.push_back("type");
+    keywords.push_back("continue");
+    keywords.push_back("for");
+    keywords.push_back("import");
+    keywords.push_back("return");
+    keywords.push_back("var");
 }
 
 /*
  * Checks if a word is in the keywords vector or not
  */
 bool Lexer::isKeyword(std::string word) {
-    for (unsigned i = 0; i < keywords.size(); i++) {
+    for (int i = 0; i < keywords.size(); i++) {
         if (keywords[i] == word) return true;
     }
     return false;
+}
+
+/*
+ * Checks if a character is a simple operator
+ */
+bool Lexer::isSimpleOperator(char c) {
+    // handle stuff here
+}
+
+/*
+ * Checks if a Operator of more Characters is valid, in operatos vector
+ */
+bool Lexer::isOperator(std::string word) {
+    // handle stuff here
+}
+
+/*
+ * Initialize the operators vector
+ */
+void Lexer::initOperators() {
+    operators.clear();
 }
 
 /*
@@ -221,7 +290,7 @@ bool Lexer::isSpace(char c) {
  */
 bool Lexer::isLetter(char c) {
     int ascii = (int) c;
-    if ((65 <= ascii && ascii <= 90) || (97 <= ascii && ascii <= 122)) return true;
+    if ((65 <= ascii && ascii <= 90) || (97 <= ascii) && (ascii <= 122)) return true;
     else return false;
 }
 
@@ -246,6 +315,6 @@ bool Lexer::isLetterOrNumber(char c) {
  * Checks if a character is a bracket
  */
 bool Lexer::isBracket(char c) {
-    if (c == '[' || c == ']' || c == '(' || c == ')' || c == '{' || c == '}' || c == ';' || c == '.') return true;
+    if (c == '[' || c == ']' || c == '(' || c == ')' || c == '{' || c == '}') return true;
     else return false;
 }
